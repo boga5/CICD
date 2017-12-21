bc0baed0ba0d048a644df9eb961a2f9b02fd523c
SnehaKailasa <sneha.kailasa@ggktech.com>
2017-12-21 17:11:09 +0530

    Update run.sh

--- RFW/run.sh
+++ RFW/run.sh
@@ -8,17 +8,17 @@
 #sh /robot/robot.sh cpserver 8080 cicd_sample/cicd_sample@52.67.80.187:1521/GGKF
 #!/bin/bash
 count=0
-while [[ ! nc -z cpservice 8080 ] -a [ $count -le 180 ]];
-        do
-          echo sleeping;
-          sleep 5;
-          count=`expr $count + 1`
-        done;
-        echo "connected Sneha"
+while [ ! nc -z cpservice 8080 -a $count -le 180 ]
+do
+ echo sleeping;
+ sleep 5;
+ count=`expr $count + 1`
+done
+echo "connected Sneha"
 if [ $count -le 180 ]; then
-        sh /robot/robot.sh cpserver 8080 cicd_sample/cicd_sample@52.67.80.187:1521/GGKF
+   sh /robot/robot.sh cpserver 8080 cicd_sample/cicd_sample@52.67.80.187:1521/GGKF
 else
-        exit 1
+   exit 1
 fi
 #while true
 #do
