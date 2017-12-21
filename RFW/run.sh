de8591ef95374f00012d2c5eb016f3cc89dbc5fe
SnehaKailasa <sneha.kailasa@ggktech.com>
2017-12-21 14:48:21 +0530

    Update run.sh

--- RFW/run.sh
+++ RFW/run.sh
@@ -1,14 +1,28 @@
+##!/bin/bash
+#while ! nc -z cpservice 8080;
+#        do
+#          echo sleeping;
+#          sleep 5;
+#        done;
+#        echo connected;
+#sh /robot/robot.sh cpserver 8080 cicd_sample/cicd_sample@52.67.80.187:1521/GGKF
 #!/bin/bash
-while ! nc -z cpservice 8080;
+declare -i count = 0
+while [ ! nc -z cpservice 8080 ] -a [ $count -le 180 ] ;
         do
           echo sleeping;
           sleep 5;
+          count = $count+1
         done;
-        echo connected;
-sh /robot/robot.sh cpserver 8080 cicd_sample/cicd_sample@52.67.80.187:1521/GGKF
+if [ $count -le 180 ]; then
+        sh /robot/robot.sh cpserver 8080 cicd_sample/cicd_sample@52.67.80.187:1521/GGKF
+else
+        exit 1
+fi
 #while true
 #do
 #	echo "Press [CTRL+C] to stop.."
 #	sleep 1
 #done 
+
 
