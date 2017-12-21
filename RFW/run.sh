5a0eff37060421cf8c791eefed11755244247aca
SnehaKailasa <sneha.kailasa@ggktech.com>
2017-12-21 18:56:10 +0530

    Update run.sh

--- RFW/run.sh
+++ RFW/run.sh
@@ -17,7 +17,6 @@ do
   count=`expr $count + 1`
  fi
 done
-echo "connected Sneha"
 if [ $count -le 180 ]; then
    sh /robot/robot.sh cpserver 8080 cicd_sample/cicd_sample@52.67.80.187:1521/GGKF
 else
